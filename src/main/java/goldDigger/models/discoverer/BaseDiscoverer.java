bb0342846f5da37b8a6361aecd60066e26878a8b
Vladimir Zorov <vl.zorov@gmail.com>
2023-01-08 11:37:38 +0200

    add changes

--- src/main/java/goldDigger/models/discoverer/BaseDiscoverer.java
+++ src/main/java/goldDigger/models/discoverer/BaseDiscoverer.java
@@ -3,6 +3,9 @@ package goldDigger.models.discoverer;
 import goldDigger.models.museum.BaseMuseum;
 import goldDigger.models.museum.Museum;
 
+import static goldDigger.common.ExceptionMessages.DISCOVERER_ENERGY_LESS_THAN_ZERO;
+import static goldDigger.common.ExceptionMessages.DISCOVERER_NAME_NULL_OR_EMPTY;
+
 public abstract class BaseDiscoverer implements Discoverer {
 
     private String name;
@@ -10,9 +13,23 @@ public abstract class BaseDiscoverer implements Discoverer {
     private Museum museum;
 
     public BaseDiscoverer(String name, double energy) {
-        this.name = name;
-        this.energy = energy;
+        setName(name);
+        setEnergy(energy);
         this.museum = new BaseMuseum();
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(DISCOVERER_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    public void setEnergy(double energy) {
+        if (energy < 0) {
+            throw new IllegalArgumentException(DISCOVERER_ENERGY_LESS_THAN_ZERO);
+        }
+       this.energy = energy;
     }
 
     @Override
